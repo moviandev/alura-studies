4f23811d26fccd662c1d0108498029174ecf9785
Matheus Viana <matheus_o_viana@hotmail.com>
2022-06-19 22:42:14 -0300

    created item component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
@@ -16,10 +16,10 @@ function List() {
       <h2>Estudos do dia</h2>
       <ul>
         {tasks.map((item, index) => (
-          <li className={style.item} key={index}>
-            <h3>{item.task}</h3>
-            <span>{item.time}</span>
-          </li>
+          <Item
+            key={index}
+            {...item}
+          />
         ))}
       </ul>
     </aside>
